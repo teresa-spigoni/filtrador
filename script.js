0e3da3b93b4b5a8c88ace6099e7025344079894c
teresa-spigoni <teresaspigoni@gmail.com>
2021-05-14 23:48:43 +0200

    fix js

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
+// todo:
+// aggiungere il pulsante download, 
+// percentuale sui filtri,
+// far comparire il range solo al click del nome del filtro,
+// far eliminare la foto,
+// mettere una foto di default,
+// fare un menu con immagini già modificate.. (?)
+
+let sepia = document.getElementById('sepia');
+let saturate = document.getElementById('saturate');
+let opacity = document.getElementById('opacity');
+let invert = document.getElementById('invert');
+let hueRotate = document.getElementById('hue-rotate');
+let grayscale = document.getElementById('grayscale');
+let contrast = document.getElementById('contrast');
+let brightness = document.getElementById('brightness');
+let blur = document.getElementById('blur');
+let img = document.getElementById('img');
+
+function filter(nomeFiltro) {
+    if (nomeFiltro === 'hue-rotate') {
+        img.style.filter = `${nomeFiltro}(${hueRotate.value}deg)`;
+    } else if (nomeFiltro === 'blur') {
+        img.style.filter = `${nomeFiltro}(${blur.value}px)`;
+    } else {
+        img.style.filter = `${nomeFiltro}(${document.getElementById(nomeFiltro).value}%)`;
+    }
+}
+
+function filterDefault() {
+    img.style.filter = 'none';
+
+    sepia.value = 0;
+    invert.value = 1;
+    hueRotate.value = 0;
+    grayscale.value = 0;
+    blur.value = 0;
+    saturate.value = 100;
+    contrast.value = 100;
+    brightness.value = 100;
+    opacity.value = 100;
+}
 
 function addImg() {
     const filters = document.getElementsByClassName('filters-container')[0];
-    const img = document.getElementById('img');
     const file = document.querySelector('input[type=file]').files[0];
     const reader = new FileReader();
 
@@ -16,57 +57,4 @@ function addImg() {
     }
 
     filterDefault();
-}
-
-function filter() {
-    let sepia = document.getElementById('sepia');
-    let saturate = document.getElementById('saturate');
-    let opacity = document.getElementById('opacity');
-    let invert = document.getElementById('invert');
-    let hueRotate = document.getElementById('hue-rotate');
-    let grayscale = document.getElementById('grayscale');
-    let contrast = document.getElementById('contrast');
-    let brightness = document.getElementById('brightness');
-    let blur = document.getElementById('blur');
-
-    let img = document.getElementById('img');
-
-    img.style.filter =
-        `
-        sepia(${sepia.value}%)
-        invert(${invert.value}%) 
-        hue-rotate(${hueRotate.value}deg) 
-        grayscale(${grayscale.value}%) 
-        blur(${blur.value}px)
-        saturate(${saturate.value}%) 
-        contrast(${contrast.value}%) 
-        brightness(${brightness.value}%) 
-        opacity(${opacity.value}%) 
-        `;
-}
-
-function filterDefault() {
-    let sepia = document.getElementById('sepia');
-    let saturate = document.getElementById('saturate');
-    let opacity = document.getElementById('opacity');
-    let invert = document.getElementById('invert');
-    let hueRotate = document.getElementById('hue-rotate');
-    let grayscale = document.getElementById('grayscale');
-    let contrast = document.getElementById('contrast');
-    let brightness = document.getElementById('brightness');
-    let blur = document.getElementById('blur');
-
-    let img = document.getElementById('img');
-
-    img.style.filter = 'none';
-
-    sepia.value = 0;
-    invert.value = 1;
-    hueRotate.value = 0;
-    grayscale.value = 0;
-    blur.value = 0;
-    saturate.value = 100;
-    contrast.value = 100;
-    brightness.value = 100;
-    opacity.value = 100;
 }
